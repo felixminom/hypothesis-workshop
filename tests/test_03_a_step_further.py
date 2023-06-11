d935c4a03f8703893c782a58568b3c180ab499ee
Felix Miño <fmino@Administrators-MacBook-Pro.local>
2023-06-11 08:11:36 -0500

    Generating custom data

--- tests/test_03_a_step_further.py
+++ tests/test_03_a_step_further.py
@@ -39,3 +39,37 @@ def test_basic_tuple():
 def test_tuple_snd_is_greater():
     # Your assert(s) here
     pytest.skip()
+
+
+# Let's discover how to generate arbitrary data for our own data types.
+# We have a `Person` class which holds an id(int) and the person's name (string).
+# First let's write this as a dataclass.
+
+# Your dataclass here
+
+# Exercise 5: Write a strategy that lets you generate data of the Person type.
+# Write your asserts too.
+def test_person_generation():
+    # Your assert(s) here
+    pytest.skip()
+
+
+# Sometimes you'd like to write your strategy once and reuse in different tests
+# with some tweaks. https://hypothesis.readthedocs.io/en/latest/data.html#hypothesis.strategies.composite
+
+# Exercise 6: Write a reusable strategy that late used in your test allows you to
+# generate id greater than a 1000 and strings that contains at least 5 elements.
+
+# Your reusable strategy here
+
+@st.composite
+def person_reusable_st(draw, id = None, name =  None):
+    # Your code goes here
+    ...
+
+# Now write your strategy using the one above and make it satisfy the two conditions
+
+# Your strategy here
+def test_person_with_conditions():
+    # Your assert(s) here
+    pytest.skip()
